ff6ac799c3a66ba0f1a09a6494a0b6b395766f42
Bhavanisingetam <singetambhavani7@gmail.com>
2020-04-03 23:41:57 -0400

    StaySearch
    function to search room details

--- sandd.c
+++ sandd.c
@@ -7,7 +7,7 @@
 #include<stdlib.h>
 #include<time.h>
 
-
+void staySearch()
 void reserveRoom(char fileName[30]);
 
 struct CustomerDetails   //STRUCTURE DECLARATION
@@ -241,4 +241,38 @@ void reserveRoom(char fileName[30])
 	fclose(f);
 }
 
-
+void staySearch()
+{
+system("cls");
+FILE *f;
+char roomno[20];
+int flag=1;
+f=fopen("add.txt","r+");
+if(f==0)
+exit(0);
+fflush(stdin);
+printf("Enter Room number of the customer to search its details: \n");
+scanf("%s", roomnumber);
+while(fread(&s,sizeof(s),1,f)==1)
+{
+if(strcmp(s.roomnumber,roomnumber)==0){
+flag=0;
+printf("\n\tRecord Found\n ");
+printf("\nRoom Number:\t%s",s.roomnumber);
+printf("\nName:\t%s",s.name);
+printf("\nAddress:\t%s",s.address);
+printf("\nPhone number:\t%s",s.phonenumber);
+printf("\nNationality:\t%s",s.nationality);
+printf("\nEmail:\t%s",s.email);
+printf("\nPeriod:\t%s",s.period);
+printf("\nArrival date:\t%s",s.arrivaldate);
+flag=0;
+break;
+}
+}
+if(flag==1){
+printf("\n\tRequested Customer could not be found!");
+}
+getch();
+fclose(f);
+}
